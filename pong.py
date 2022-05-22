e86fee997a31ab6a2b14e183c90341fa095b18d6
isaaccavallaro <isaaccavallaro@gmail.com>
2022-05-22 13:39:56 +1000

    Getting both paddles to move when certain keys are pressed

--- pong.py
+++ pong.py
@@ -32,6 +32,37 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+# Move paddle_one up by 20 pixels
+def paddle_one_up():
+    y = paddle_one.ycor()
+    y += 20
+    paddle_one.sety(y)
+
+# Move paddle_one down by 20 pixels
+def paddle_one_down():
+    y = paddle_one.ycor()
+    y -= 20
+    paddle_one.sety(y)
+
+# Move paddle_two up by 20 pixels
+def paddle_two_up():
+    y = paddle_two.ycor()
+    y += 20
+    paddle_two.sety(y)
+
+# Move paddle_two down by 20 pixels
+def paddle_two_down():
+    y = paddle_two.ycor()
+    y -= 20
+    paddle_two.sety(y)
+
+# Keyboard binding
+window.listen()
+window.onkeypress(paddle_one_up, "w") # Call paddle_one_up when w is pressed
+window.onkeypress(paddle_one_down, "s") # Call paddle_one_up when s is pressed
+window.onkeypress(paddle_two_up, "Up") # Call paddle_two_up when the up arrow is pressed
+window.onkeypress(paddle_two_down, "Down") # Call paddle_two_up when the down arrow is pressed
+
 
 # Main game loop
 while True:
