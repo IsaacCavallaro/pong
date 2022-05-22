47af22bb1e430dbf74df4ce604ef9d6dca51163e
isaaccavallaro <isaaccavallaro@gmail.com>
2022-05-22 13:24:36 +1000

    Adding first paddle

--- pong.py
+++ pong.py
@@ -6,4 +6,19 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Padding One
+paddle_one = turtle.Turtle()
+paddle_one.speed(0)
+paddle_one.shape("square")
+paddle_one.color("white")
+paddle_one.penup()
+paddle_one.goto(-350, 0)
+
+
+# Paddle Two
+
+# Ball
+
 # Main game loop
+while True:
+    window.update()
