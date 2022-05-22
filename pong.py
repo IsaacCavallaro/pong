09cc53e48a4b7921b157b143ec2455e890587b47
isaaccavallaro <isaaccavallaro@gmail.com>
2022-05-22 13:27:56 +1000

    adding padding two

--- pong.py
+++ pong.py
@@ -11,11 +11,18 @@ paddle_one = turtle.Turtle()
 paddle_one.speed(0)
 paddle_one.shape("square")
 paddle_one.color("white")
+paddle_one.shapesize(stretch_wid=5, stretch_len=1)
 paddle_one.penup()
 paddle_one.goto(-350, 0)
 
-
 # Paddle Two
+paddle_two = turtle.Turtle()
+paddle_two.speed(0)
+paddle_two.shape("square")
+paddle_two.color("white")
+paddle_two.shapesize(stretch_wid=5, stretch_len=1)
+paddle_two.penup()
+paddle_two.goto(350, 0)
 
 # Ball
 
